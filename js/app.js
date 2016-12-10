8ef36c5e0bfd58a73529476d2cc8784b0dd39d6c
João Victor <contatos.jv@hotmail.com>
2016-12-10 17:51:12 -0200

    enemy improvements

--- js/app.js
+++ js/app.js
@@ -23,17 +23,67 @@ Enemy.prototype.update = function(dt, pause) {
 
         // make enemies loop to left side of canvas after reaching canvas.width
         if (this.x >= 505) {
-            this.x = 0;
+            this.x = -100;
         }
 
         // Check for collision with enemies or barrier-walls
-        checkCollision(this);
+        enemy.checkCollision(this);
     } else {
         this.x = this.x;
     }
 
-
 };
+
+Enemy.prototype.checkCollision =  function(enemy){
+        // check for collision between enemy and player
+    if (
+        player.y + 131 >= enemy.y + 90 &&
+        player.x + 25 <= enemy.x + 88 &&
+        player.y + 73 <= enemy.y + 135 &&
+        player.x + 76 >= enemy.x + 11) {
+        player.x = 202.5;
+        player.y = 383;
+
+        if (score === 0) {
+            score = 0;
+        } else {
+            score--;
+        }
+
+    }
+
+    // check for player reaching top of canvas and winning the game
+    // if player wins, add 1 to the score and level
+    // pass score as an argument to the increaseDifficulty function
+    if (player.y + 63 <= 0) {
+        player.x = 202.5;
+        player.y = 383;
+
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, 505, 171);
+
+        score++;
+        gameLevel++;
+        console.log('current score: ' + score + ', current level: ' + gameLevel);
+        increaseDifficulty(score);
+    }
+
+    // check if player runs into left, bottom, or right canvas walls
+    // prevent player from moving beyond canvas wall boundaries
+    if (player.y > 383) {
+        player.y = 383;
+    }
+    if (player.x > 402.5) {
+        player.x = 402.5;
+    }
+    if (player.x < 2.5) {
+        player.x = 2.5;
+    }
+}
+
+// var checkCollision = function(anEnemy) {
+
+// };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -133,53 +183,6 @@ var pauseGame = function() {
     pause = !pause;
     text = text !== textDefault ? textDefault : "Pause";
     drawText(text);
-};
-
-var checkCollision = function(anEnemy) {
-    // check for collision between enemy and player
-    if (
-        player.y + 131 >= anEnemy.y + 90 &&
-        player.x + 25 <= anEnemy.x + 88 &&
-        player.y + 73 <= anEnemy.y + 135 &&
-        player.x + 76 >= anEnemy.x + 11) {
-        player.x = 202.5;
-        player.y = 383;
-
-        if (score === 0) {
-            score = 0;
-        } else {
-            score--;
-        }
-
-    }
-
-    // check for player reaching top of canvas and winning the game
-    // if player wins, add 1 to the score and level
-    // pass score as an argument to the increaseDifficulty function
-    if (player.y + 63 <= 0) {
-        player.x = 202.5;
-        player.y = 383;
-
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, 505, 171);
-
-        score++;
-        gameLevel++;
-        console.log('current score: ' + score + ', current level: ' + gameLevel);
-        increaseDifficulty(score);
-    }
-
-    // check if player runs into left, bottom, or right canvas walls
-    // prevent player from moving beyond canvas wall boundaries
-    if (player.y > 383) {
-        player.y = 383;
-    }
-    if (player.x > 402.5) {
-        player.x = 402.5;
-    }
-    if (player.x < 2.5) {
-        player.x = 2.5;
-    }
 };
 
 // Increase number of enemies on screen based on player's score
